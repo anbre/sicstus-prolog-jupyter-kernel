307602105cc5b6b07b4a0b6bdd36961937514dbe
Brecklinghaus, Anne (anbre111) <anne.brecklinghaus@uni-duesseldorf.de>
2022-08-08 15:54:12 +0200

    Add example notebook

---
+++ notebooks/test.pl
@@ -0,0 +1,23 @@
+% This file defines PL-Unit tests.
+% It is used to show how these tests can be run from a Jupyter notebook (Using_Jupyter_Notebooks_with_SICStus_Prolog.ipynb).
+
+
+test_pred(a).
+test_pred(b).
+
+
+:- use_module(library(plunit)).
+
+
+:- begin_tests(test).
+
+test(a) :-
+  test_pred(a).
+
+test(b) :-
+  test_pred(b).
+
+test(c, [fail]) :-
+  test_pred(c).
+
+:- end_tests(test).
